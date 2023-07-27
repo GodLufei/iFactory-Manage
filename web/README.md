76bd12da157396ed4e7a307525354b0da0171ac5
GodLufei <370933147@qq.com>
2023-07-27 15:33:48 +0800

    init frontend with vuie-pure-admin

--- web/README.md
+++ web/README.md
@@ -1,1 +1,6 @@
-IFactory-Manage Frontend
+- [查看文档](https://yiming_chang.gitee.io/pure-admin-doc)
+
+```shell
+  pnpm install
+  pnpm dev
+```
---
+++ web/src/views/welcome/index.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+defineOptions({
+  name: "Welcome"
+});
+</script>
+
+<template>
+  <h1>IFactory Management System</h1>
+</template>
