c8d7995ed40972179d5eb71e536a6997e436457e
GodLufei <370933147@qq.com>
2023-08-11 23:52:01 +0800

    update create page

--- web/src/views/product/create/index.vue
+++ web/src/views/product/create/index.vue
@@ -4,34 +4,61 @@
       <BasicForm @register="register" />
     </a-card>
     <a-card title="产品项" :bordered="false">
-      产品项
+      <BasicTable @register="registerTable" />
+      <a-button block class="mt-5" @click="addProductItem"> 新增产品项 </a-button>
     </a-card>
     <a-card :bordered="false" :align="'center'" class="!mt-5 center">
-      <a-button type="info" @click="console.log('click')"> 重置 </a-button>
-      <a-button type="primary" @click="console.log('click')" class="!ml-5"> 提交 </a-button>
+      <a-button type="info" @click="resetInput">重置</a-button>
+      <a-button type="primary" @click="createProduct" class="!ml-5">提交</a-button>
     </a-card>
   </PageWrapper>
 </template>
 <script lang="ts">
-  import { defineComponent } from 'vue';
   import { BasicForm, useForm } from '/@/components/Form';
+  import { BasicTable, useTable } from '/@/components/Table';
   import { PageWrapper } from '/@/components/Page';
   import { Card, Space } from 'ant-design-vue';
-  import { schemas } from './data';
+  import { productSchemas, productItemTableSchemas } from './data';
+  import { defineComponent } from 'vue';
   export default defineComponent({
     name: 'CreateProductPage',
-    components: { BasicForm, PageWrapper, [Card.name]: Card, [Space.name]: Space },
+    components: {
+      BasicForm,
+      PageWrapper,
+      BasicTable,
+      [Card.name]: Card,
+      [Space.name]: Space,
+    },
     setup() {
       const [register] = useForm({
         layout: 'vertical',
         baseColProps: {
           span: 6,
         },
-        schemas: schemas,
+        schemas: productSchemas,
         showActionButtonGroup: false,
       });
+      const [registerTable] = useTable({
+        columns: productItemTableSchemas,
+        showIndexColumn: true,
+        scroll: { y: 100 },
+        pagination: false,
+      });
+      const addProductItem = () => {
+        alert('OK');
+      };
+      const createProduct = () => {
+        alert('OK');
+      };
+      const resetInput = () => {
+        alert('OK');
+      };
       return {
         register,
+        registerTable,
+        addProductItem,
+        createProduct,
+        resetInput,
       };
     },
   });
